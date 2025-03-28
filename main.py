f09ae19fa461d70d5b1a86a99df95f0c4367cb99
Axel Muñiz Tello <axeltello@Mac.lan>
2025-03-28 01:50:28 -0700

    New Updates 150++

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ Description: This script will be used to simulate the population dynamics of bob
              for the applied mathematics challenge at UC Merced (2025)
 Note: Please refer to the README.md file for more information on the project and the workings of the code.
 """
-# Imported Libraries: 
-import numpy as np
-import pandas as pd
-import os 
+
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.integrate import solve_ivp
+import numpy as np
+import pandas as pd
+import os 
 
 if not os.path.exists('plots'):
     os.makedirs('plots')
@@ -27,7 +27,7 @@ def predator_prey (t, z, a, b, c, d):
 bobcat_data = pd.read_csv('data/bobcatData.csv')
 rabbit_data = pd.read_csv('data/rabbitData.csv')
 
-# Clean Data:
+# Organizing Data:
 rabbit_data.columns = ['Day', 'Rabbits']
 bobcat_data.columns = ['Day', 'Bobcats']
 
@@ -38,20 +38,20 @@ x_values = rabbit_data['Rabbits'].values
 y_values = bobcat_data['Bobcats'].values
 
 # Plot for Rabbit Population Over Time:
-rabbit_data.plot(s=5, x='Day', y='Rabbits', kind='scatter') 
+rabbit_data.plot.scatter(x='Day', y='Rabbits', s=5) 
 plt.title('Rabbit Population Over Time') 
 plt.xlabel('Day')
 plt.ylabel('Population')
 plt.savefig('plots/rabbit_population_plot.png')
-plt.show()
+plt.close()
 
 # Plot for Bobcat Population Over Time:
-bobcat_data.plot(s = 5, x='Day', y='Bobcats', kind='scatter')  
+bobcat_data.plot.scatter(x='Day', y='Bobcats', s=5)  
 plt.title('Bobcat Population Over Time')
 plt.xlabel('Day')
 plt.ylabel('Population')
 plt.savefig('plots/bobcat_population_plot.png') 
-plt.show()
+plt.close()
 
 ######-BEGINNING OF METHOD-######
 """
@@ -69,8 +69,8 @@ t_common = np.linspace(t_min, t_max, 200)
 interp_rabbit = interp1d(t_rabbit, x_values, kind='linear', fill_value='extrapolate')
 interp_bobcat = interp1d(t_bobcat, y_values, kind='linear', fill_value='extrapolate')
 
-x_interp = interp_rabbit(t_common)
-y_interp = interp_bobcat(t_common)
+x_interp = interp_rabbit(t_common) # x_interp = rabbit population => x(t)
+y_interp = interp_bobcat(t_common) # y_interp = bobcat population => y(t)
 
 # Plot for Interpolated Rabbit Population:
 plt.plot(t_common, x_interp, label='Interpolated Rabbits')
@@ -81,4 +81,4 @@ plt.ylabel("Population")
 plt.legend()
 plt.grid(True)
 plt.savefig("plots/interpolated_data.png")
-plt.show()
+plt.close()
